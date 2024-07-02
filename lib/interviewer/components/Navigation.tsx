7ea8608aabd83064861ca73c65db4d4b35166e6e
Caden Buckhalt <halaeb@gmail.com>
2024-07-02 10:58:57 -0700

    example of accessible stage nav buttons
    includes adding ability to navigate between stages using keyboard and tab + enter, focus states on the ui components, and aria labels.

--- lib/interviewer/components/Navigation.tsx
+++ lib/interviewer/components/Navigation.tsx
@@ -1,34 +1,37 @@
-import ProgressBar from '~/lib/ui/components/ProgressBar';
 import { ChevronDown, ChevronUp } from 'lucide-react';
-import { cn } from '~/utils/shadcn';
 import { useSelector } from 'react-redux';
+import ProgressBar from '~/lib/ui/components/ProgressBar';
+import { cn } from '~/utils/shadcn';
 import { getNavigationInfo } from '../selectors/session';
 
 const NavigationButton = ({
   disabled,
   onClick,
   className,
+  ariaLabel,
   children,
 }: {
   disabled?: boolean;
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  ariaLabel?: string;
   children: React.ReactNode;
 }) => {
   return (
-    <div
+    <button
       className={cn(
         `session-navigation__button m-4 flex h-[4.8rem] w-[4.8rem] basis-[4.8rem] cursor-pointer items-center justify-center rounded-full transition-all`,
         'hover:bg-[#4a4677]',
         disabled && 'cursor-not-allowed opacity-50 hover:bg-transparent',
+        'focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
         className,
       )}
-      role="button"
-      tabIndex={0}
+      tabIndex={disabled ? -1 : 0}
       onClick={!disabled ? onClick : undefined}
+      aria-label={ariaLabel}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
@@ -57,6 +60,7 @@ const Navigation = ({
       <NavigationButton
         onClick={moveBackward}
         disabled={disabled || !canMoveBackward}
+        ariaLabel="Move backward"
       >
         <ChevronUp className="h-[2.4rem] w-[2.4rem]" strokeWidth="3px" />
       </NavigationButton>
@@ -71,6 +75,7 @@ const Navigation = ({
         )}
         onClick={moveForward}
         disabled={disabled || !canMoveForward}
+        ariaLabel="Move forward"
       >
         <ChevronDown className="h-[2.4rem] w-[2.4rem]" strokeWidth="3px" />
       </NavigationButton>
